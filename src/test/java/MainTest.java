736691d79f73156becd5f3789c3e365220a17850
hronograf <vlad.boholii@gmail.com>
2020-05-15 18:44:01 +0300

    added Cryptor and CRC16 tests

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,69 +2,83 @@ import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MainTest {
 
-    private static String algorithm = "DESede";
-    private static Key key;
-    private static Cipher cipher;
 
     ByteBuffer buffer;
     private static byte[] byteArr;
 
 
-
-    private static void initCipher() {
-        try {
-            cipher = Cipher.getInstance(algorithm);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void initKey() {
-        try {
-            key = KeyGenerator.getInstance(algorithm).generateKey();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
-
     @BeforeAll
-    static void init() throws DecoderException{
-        initKey();
-        initCipher();
-        PacketEncoder.setEncryption(key, cipher);
-        PacketDecoder.setEncryption(key, cipher);
-        byteArr = PacketEncoder.encode((byte)0,1, 777,21, "Hello World!");
+    static void init() {
+        byteArr = PacketEncoder.encode((byte) 0, 1, 777, 21, "Hello World!");
     }
 
-    @Test    //check for validity of CRC1 & CRC2
+    @Test
+        //check for validity of CRC1 & CRC2
     void shouldPass_whenValidInputMessage() {
         PacketDecoder.decodePacket(byteArr);
     }
 
     @Test
-    void shouldThrowException_whenInvalidMagicByte() {
-        assertThrows(IllegalArgumentException.class,
-                () -> PacketDecoder.decodePacket(Hex.decodeHex("12")
-        ));
+        //check for validity of CRC1 & CRC2
+    void shouldPass_whenValidInputMessag() {
+        PacketDecoder.decodePacket(byteArr);
     }
 
     @Test
-    void shouldPass_whenValidPacketSource() throws DecoderException {
+    void shouldThrowException_whenInvalidMagicByte() {
+        assertThrows(IllegalArgumentException.class, () -> PacketDecoder.decodePacket(Hex.decodeHex("12")));
+    }
+
+    @Test
+    void shouldPass_whenValidPacketSource() {
         buffer = ByteBuffer.wrap(byteArr);
         assertEquals(0, buffer.get(1), "Invalid packet source!");
+    }
+
+    @Test
+    void shouldPass_ifCryptorWorksNice() throws UnsupportedEncodingException {
+        String message = "message";
+
+        try {
+            assertEquals(message, new String(Cryptor.decryptMessage(Cryptor.encryptMessage(message.getBytes("UTF-8")))),
+                    "Cryptor doesn't work");
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void shouldThrowException_whenInvalid_first_CRC16() throws DecoderException {
+        byte[] packet = PacketEncoder.encode((byte) 0, 1, 777, 21, "Hello World!");
+
+        //break header
+        packet[10] = (byte) (packet[10] - 1);
+
+        assertThrows(IllegalArgumentException.class, () -> PacketDecoder.decodePacket(packet));
+    }
+
+    @Test
+    void shouldThrowException_whenInvalid_second_CRC16() throws DecoderException {
+        byte[] packet = PacketEncoder.encode((byte) 0, 1, 777, 21, "message");
+
+        //break message
+        packet[packet.length - 3] = (byte) (packet[packet.length - 3] - 1);
+
+
+        assertThrows(IllegalArgumentException.class, () -> PacketDecoder.decodePacket(packet));
     }
 
 
